387b6eb32f69c23790ec0ce043a394636db4b1a7
BigOoga <edoardo.ruffi@gmail.com>
2021-11-27 15:16:47 +0100

    Added one more user to seeder for testing purposes

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -16,7 +16,7 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->insert([
             'name' => 'Edoardo',
-            'last_name' => 'Anonymous',
+            'last_name' => 'Ruffi',
             'email' => 'edoardo.ruffi@gmail.com',
             'password' => Hash::make('password'),
             'api_token' => Str::random(80),
@@ -24,7 +24,7 @@ class UserSeeder extends Seeder
 
         DB::table('users')->insert([
             'name' => 'Andrea',
-            'last_name' => 'Anonymous',
+            'last_name' => 'P.',
             'email' => 'andrea.p@gmail.com',
             'password' => Hash::make('password'),
             'api_token' => Str::random(80),
@@ -32,8 +32,16 @@ class UserSeeder extends Seeder
 
         DB::table('users')->insert([
             'name' => 'Matteo',
-            'last_name' => 'Anonymous',
+            'last_name' => 'G.',
             'email' => 'matteo.g@gmail.com',
+            'password' => Hash::make('password'),
+            'api_token' => Str::random(80),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Max',
+            'last_name' => 'B.',
+            'email' => 'max.b@gmail.com',
             'password' => Hash::make('password'),
             'api_token' => Str::random(80),
         ]);
